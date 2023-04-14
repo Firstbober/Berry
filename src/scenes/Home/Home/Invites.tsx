f2b3a5db06b997ece7c184b5013ab731c78cff59
Firstbober <firstbober@tutanota.com>
2023-04-14 18:11:16 +0200

    UI: Invites tab WIP component

---
+++ src/scenes/Home/Home/Invites.tsx
@@ -0,0 +1,12 @@
+import { Tab } from '../../../ui/Tabs'
+import WorkInProgress from '../../../ui/WorkInProgress'
+
+const Invites = () => {
+  return (
+    <Tab>
+      <WorkInProgress />
+    </Tab>
+  )
+}
+
+export default Invites
---
+++ src/ui/WorkInProgress.tsx
@@ -0,0 +1,12 @@
+const WorkInProgress = () => {
+  return (
+    <section class="h-full w-full flex flex-col text-center justify-center items-center">
+      <img src="/images/picto/work-in-progress.svg"
+        alt="Work in progress infographic" class="max-w-xs max-h-xs min-w-0 min-h-0 mt-12" />
+      <header class="text-4xl font-semibold mb-4">Work in progress</header>
+      <p class="text-white-600 mb-auto">We're sorry, but this section of the Berry is still in the works.</p>
+    </section>
+  )
+}
+
+export default WorkInProgress

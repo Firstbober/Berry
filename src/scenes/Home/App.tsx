ad42509f450add88bded2ffe95e00cb455122fd7
Firstbober <firstbober@tutanota.com>
2023-04-20 17:37:52 +0200

    UI: Space name and settings button

--- src/scenes/Home/App.tsx
+++ src/scenes/Home/App.tsx
@@ -24,12 +24,12 @@ export const App = () => {
       <Tab>
         <section class='max-h-full h-full w-full relative flex items-center'>
           {/* Spaces bar */}
-          <section class='flex flex-col w-20 h-full border-r border-white-300 pt-3 overflow-y-auto scroll scrollbar-hide'>
+          <section class='flex flex-col w-20 min-w-[5rem] h-full border-r border-white-400 pt-3 overflow-y-auto scroll scrollbar-hide'>
             <For each={new Array(20)}>{(_, idx) =>
                 // Space
                 <button class='pl-3 pr-3 mb-3 relative group'>
                   {/* Indicator */}
-                  <div class={`absolute w-2 h-full left-0 -translate-x-full ${idx() == 0 ? '-translate-x-0' : ''} flex items-center duration-150 group-hover:-translate-x-0`}>
+                  <div class={`absolute w-2 h-full left-0 ${idx() == 0 ? '-translate-x-0' : '-translate-x-full'} flex items-center duration-150 group-hover:-translate-x-0`}>
                     <div class={`bg-gradient-to-br from-brandRed to-brandPink w-1/2 ${idx() == 0 ? 'h-5/6' : 'h-2/5'} rounded-tr rounded-br`}></div>
                   </div>
                   <img src="https://picsum.photos/200" class={`rounded object-cover ${idx() == 0 ? '' : 'saturate-[0.3] scale-90'}`} />
@@ -38,8 +38,21 @@ export const App = () => {
           </section>
 
           {/* Room list */}
-          <section class='flex grow h-full'>
+          <section class='flex h-full min-w-0 max-w-fit'>
+            {/* Space name and setting */}
+            <button class='flex p-2 pl-4 pr-4 min-w-0 items-center h-14 border-b border-white-300'>
+              <span class='overflow-hidden overflow-ellipsis whitespace-nowrap max-w-full
+                font-semibold text-lg'>Berry Matrix Client: Rayman Origins: Electic Boogaloo 2</span>
+              <div class='grow flex w-12 h-full items-center justify-center ml-6'>
+                <img src="/icons/remixicon/more-fill.svg" alt="Show space info and settings"
+                  class='w-6 h-6' />
+              </div>
+            </button>
 
+            {/* Rooms */}
+            <section>
+
+            </section>
           </section>
         </section>
       </Tab>
